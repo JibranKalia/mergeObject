650a0599982b5b459cdcec3ab60f425da61692ee
Jibran Kalia <jibran.kalia@gmail.com>
2017-12-15 16:35:15 -0800

    Non-recursive merge works

--- merge.js
+++ merge.js
@@ -1,8 +1,8 @@
 
 /**
  * Helper function to ensure passed in paramter is a function.
- * https://stackoverflow.com/questions/5999998/how-can-i-check-if-a-javascript-variable-is-function-type 
- * @param {*} obj 
+ * https://stackoverflow.com/questions/5999998/how-can-i-check-if-a-javascript-variable-is-function-type
+ * @param {*} obj
  */
 
 const isFunction = (functionToCheck) => {
@@ -10,12 +10,21 @@ const isFunction = (functionToCheck) => {
   return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
 };
 
+
+/**
+ * Merge function does a recursive merge of two objects.
+ * In case of collision, it uses the decision func if present otherwise it uses src1's value
+ * @param {object} src1
+ * @param {object} src2
+ * @param {function} decisionFunc
+ */
+
 const merge = (src1, src2, decisionFunc) => {
   if (Array.isArray(src1) || Array.isArray(src2)) {
     throw new Error('Merge only accepts Objects');
   }
-  if (decisionFunc && isFunction(decisionFunc)) {
-    throw new Error('decision Func has to be an object');
+  if (decisionFunc && !isFunction(decisionFunc)) {
+    throw new Error('decision Func has to be a Function');
   }
   const output = {};
   Object.keys(src1).forEach((key) => {
@@ -34,7 +43,6 @@ const merge = (src1, src2, decisionFunc) => {
 };
 
 
-
 const a = {
   x: 3,
   y: 4,
@@ -44,13 +52,7 @@ const b = {
 };
 
 const sum = (x, y) => x + y;
-// const expected = {
-//   x: 3,
-//   y: 3,
-// };
-
 
 console.log(merge(a, b, sum));
-
 
 module.exports = merge;
